6db7ffd5b0b1562a97dafbdef80047063a451dc1
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-11-21 21:51:40 +0000

    initial spring boot setup

---
+++ src/main/java/com/vaicomtudo/backend/VaiComTudoApplication.java
@@ -0,0 +1,13 @@
+package com.vaicomtudo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaiComTudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaiComTudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaicomtudo/backend/VaiComTudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaicomtudo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaiComTudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
