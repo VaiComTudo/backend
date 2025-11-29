41b9644d94672332e8844d6758e0224728888881
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-11-29 22:59:31 +0000

    another fix

--- src/test/java/com/vaicomtudo/backend/VaiComTudoApplicationTests.java
+++ src/test/java/com/vaicomtudo/backend/VaiComTudoApplicationTests.java
@@ -1,16 +1,8 @@
 package com.vaicomtudo.backend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import app.getxray.xray.junit.customjunitxml.annotations.Requirement;
 
 @SpringBootTest
 class VaiComTudoApplicationTests {
-
-	@Test
-	@Requirement("VCT-48")
-	void contextLoads() {
-	}
 
 }
