99015695d0f30bfb760d572f98903838bb8604c1
Apmds <ap1mrfj@gmail.com>
2025-11-29 13:35:06 +0000

    VCT-48 Entity start

---
+++ src/main/java/com/vaicomtudo/backend/data/Account.java
@@ -0,0 +1,43 @@
+package com.vaicomtudo.backend.data;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    @Getter
+    @Setter
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank
+    @Email
+    @Getter
+    @Setter
+    private String email;
+
+    @Column(nullable = false)
+    @Getter
+    @Setter
+    private String passwordHash;
+}
---
+++ src/main/java/com/vaicomtudo/backend/data/Listing.java
@@ -0,0 +1,64 @@
+
+package com.vaicomtudo.backend.data;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Listing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Getter
+    @Setter
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    @Getter
+    @Setter
+    private User owner;
+
+    @Column(nullable = false)
+    @Getter
+    @Setter
+    private String description;
+
+    @Column(nullable = false)
+    @Getter
+    @Setter
+    private String title;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    @Getter
+    @Setter
+    private BigDecimal price;
+
+
+    @ElementCollection
+    @CollectionTable(
+        name = "listing_availability",
+        joinColumns = @JoinColumn(name = "listing_id")
+    )
+    private Set<AvailabilityPeriod> availability = new HashSet<>();
+}
