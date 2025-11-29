6d06b3059ef27aaa0e7efbfe4c3b19200c78be36
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-11-29 22:56:32 +0000

    another fix

--- src/test/java/com/vaicomtudo/backend/VaiComTudoApplicationTests.java
+++ src/test/java/com/vaicomtudo/backend/VaiComTudoApplicationTests.java
@@ -2,13 +2,14 @@ package com.vaicomtudo.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import app.getxray.xray.junit.customjunitxml.annotations.XrayTest;
+
+import app.getxray.xray.junit.customjunitxml.annotations.Requirement;
 
 @SpringBootTest
 class VaiComTudoApplicationTests {
 
 	@Test
-	@XrayTest(key = "VCT-48")
+	@Requirement("VCT-48")
 	void contextLoads() {
 	}
 
