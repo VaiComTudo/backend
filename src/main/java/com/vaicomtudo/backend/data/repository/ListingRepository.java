247f0fe40d1a53763dd9c305a301b60f207be6e1
Gonçalo Simões <151958998+goncaloosimoes@users.noreply.github.com>
2025-12-07 23:30:12 +0000

    VCT-33 run tests with gui to see whats going on

--- src/main/java/com/vaicomtudo/backend/data/repository/ListingRepository.java
+++ src/main/java/com/vaicomtudo/backend/data/repository/ListingRepository.java
@@ -14,11 +14,19 @@ import com.vaicomtudo.backend.data.entity.User;
 
 @Repository
 public interface ListingRepository extends JpaRepository<Listing, UUID> {
-    List<Listing> findByStateAndVehicleType(ListingState state, String type);
-    List<Listing> findByState(ListingState state);
-    List<Listing> findByStateAndPickUpLocationContainingIgnoreCase(ListingState state, String location);
-    List<Listing> findByStateAndDropOffLocationContainingIgnoreCase(ListingState state, String location);
-    List<Listing> findByStateAndVehicleTypeAndPickUpLocationContainingIgnoreCase(ListingState state, String type, String location);
-    List<Listing> findByStateAndVehicleTypeAndDropOffLocationContainingIgnoreCase(ListingState state, String type, String location);
-    Page<Listing> findByOwner(User owner, Pageable pageable);
+        List<Listing> findByStateAndVehicleType(ListingState state, String type);
+
+        List<Listing> findByState(ListingState state);
+
+        List<Listing> findByStateAndPickUpLocationContainingIgnoreCase(ListingState state, String location);
+
+        List<Listing> findByStateAndDropOffLocationContainingIgnoreCase(ListingState state, String location);
+
+        List<Listing> findByStateAndVehicleTypeAndPickUpLocationContainingIgnoreCase(ListingState state, String type,
+                        String location);
+
+        List<Listing> findByStateAndVehicleTypeAndDropOffLocationContainingIgnoreCase(ListingState state, String type,
+                        String location);
+
+        Page<Listing> findByOwner(User owner, Pageable pageable);
 }
