e6afe858fac69e220f9a43c55a192769bd757f83
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-12-09 15:32:19 +0000

    prometheus and grafana working

--- src/main/java/com/vaicomtudo/backend/configs/SecurityConfig.java
+++ src/main/java/com/vaicomtudo/backend/configs/SecurityConfig.java
@@ -30,7 +30,8 @@ public class SecurityConfig {
                     "/api/v1/auth/**",
                     "/swagger-ui/**",
                     "/v3/api-docs/**",
-                    "/swagger-ui.html"
+                    "/swagger-ui.html",
+                    "/actuator/**"
                 ).permitAll()
                 .anyRequest().authenticated())
             .sessionManagement(session -> session
