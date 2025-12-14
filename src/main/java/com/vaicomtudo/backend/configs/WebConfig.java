4007a3d929c195fb0a76cd90b95e9853649b7965
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2025-12-14 10:54:57 +0000

    trying to fix cors to work in the vm

--- src/main/java/com/vaicomtudo/backend/configs/WebConfig.java
+++ src/main/java/com/vaicomtudo/backend/configs/WebConfig.java
@@ -13,8 +13,12 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:5173")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://deti-tqs-05:5173",
+                        "http://deti-tqs-05.ua.pt:5173"
+                    )
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
             }
